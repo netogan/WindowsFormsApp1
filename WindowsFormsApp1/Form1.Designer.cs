e292323e3101c7c578efb907c0f442f5798a5554
geraldo <geraldo@actuar.group>
2018-12-04 18:11:06 -0200

    funcionando face detection

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 27);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(415, 411);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Cuda;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using Emgu.CV.UI;
+using Emgu.CV.Cuda;
+using FaceDetection;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var opt = new OpenFileDialog();
+
+            if (opt.ShowDialog(this) == DialogResult.OK)
+            {
+                imageBox1.Image = GetFacePoints(opt.FileName);
+            }
+        }
+
+        public Image<Bgr, Byte> GetFacePoints(string fileName)
+        {
+            //var faceDetector = new CascadeClassifier(@"c:\\users\\acdev\\source\\repos\\WindowsFormsApp1\\WindowsFormsApp1\\haarcascade_frontalface_alt2.xml");
+
+            IImage image;
+
+            image = new UMat(fileName, ImreadModes.Color);
+
+            long detectionTime;
+
+            var faces = new List<Rectangle>();
+            var eyes = new List<Rectangle>();
+
+            DetectFace.Detect(image, "c:\\users\\acdev\\source\\repos\\WindowsFormsApp1\\WindowsFormsApp1\\haarcascade_frontalface_default.xml",
+                "c:\\users\\acdev\\source\\repos\\WindowsFormsApp1\\WindowsFormsApp1\\haarcascade_eye.xml", faces, eyes, out detectionTime);
+
+            foreach (Rectangle face in faces)
+                CvInvoke.Rectangle(image, face, new Bgr(Color.Red).MCvScalar, 2);
+            foreach (Rectangle eye in eyes)
+                CvInvoke.Rectangle(image, eye, new Bgr(Color.Blue).MCvScalar, 2);
+
+
+
+
+
+            //using (Image<Bgr, byte> nextFrame = cap.QueryFrame())
+            //{
+            //    if (nextFrame != null)
+            //    {
+            //        // there's only one channel (greyscale), hence the zero index
+            //        //var faces = nextFrame.DetectHaarCascade(haar)[0];
+            //        Image<Gray, byte> grayframe = nextFrame.Convert<Gray, byte>();
+            //        var faces =
+            //            grayframe.DetectHaarCascade(
+            //                haar, 1.4, 4,
+            //                HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+            //                new Size(nextFrame.Width / 8, nextFrame.Height / 8)
+            //            )[0];
+            //        foreach (var face in faces)
+            //        {
+            //            nextFrame.Draw(face.rect, new Bgr(0, double.MaxValue, 0), 3);
+            //        }
+            //        pictureBox1.Image = nextFrame.ToBitmap();
+            //    }
+            //}
+
+
+            //var fParams = new FacemarkLBFParams
+            //{
+            //    NLandmarks = 300,
+            //    InitShapeN = 10,
+            //    StagesN = 5,
+            //    TreeN = 6,
+            //    TreeDepth = 5,
+            //    ModelFile = @"c:\\users\\acdev\\source\\repos\\WindowsFormsApp1\\WindowsFormsApp1\\lbfmodel.yaml"
+            //};
+
+            //var facemark = new FacemarkLBF(fParams);
+
+            //var image = new Image<Bgr, byte>(fileName);
+            //var grayImage = image.Convert<Gray, byte>();
+
+            //grayImage._EqualizeHist();
+
+            //var faces = new VectorOfRect(faceDetector.DetectMultiScale(grayImage));
+
+            //var landmarks = new VectorOfVectorOfPointF();
+
+            //facemark.LoadModel(fParams.ModelFile);
+
+            //var success = facemark.Fit(grayImage, faces, landmarks);
+
+            //if (!success) return null;
+
+            //var facesRect = faces.ToArray();
+
+            //foreach (var face in facesRect)
+            //{
+            //    image.Draw(face, new Bgr(Color.Red), 2);
+            //}
+
+            //for (var i = 0; i < facesRect.Length; i++)
+            //{
+            //    image.Draw(facesRect[i], new Bgr(Color.Red), 2);
+            //    //FaceInvoke.DrawFacemarks(image, landmarks[i], new Bgr(Color.Blue).MCvScalar);
+            //}
+
+            return new Image<Bgr, byte>(image.Bitmap);
+        }
+    }
+}
